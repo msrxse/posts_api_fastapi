8d6fd9a497d983161d87e79318816fb8a0dd43e1
msr <msrxse@gmail.com>
2024-09-23 14:24:54 +0100

    Add pydantic model

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+# Pydantic models
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # is optional and defaults to true
+    rating: Optional[int] = None  # optional but without default
 
 
 # request Get method URL: "/"
@@ -13,3 +23,9 @@ def read_root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is a list posts"}
+
+
+@app.post("/createposts")
+def create_post(post: Post):
+    print(post.model_dump())  # This converts the pydantic model into a dictionary
+    return {"data": post}
