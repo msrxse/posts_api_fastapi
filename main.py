9384748b9c4db611a9dc483a9d05572a33d9c841
msr <msrxse@gmail.com>
2024-09-23 17:53:04 +0100

    Save to internal memory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -13,6 +14,24 @@ class Post(BaseModel):
     rating: Optional[int] = None  # optional but without default
 
 
+my_posts = [
+    {
+        "title": "Title of the post",
+        "content": "This is the content of the post",
+        "published": False,
+        "rating": None,
+        "id": 1,
+    },
+    {
+        "title": "Title of the second post",
+        "content": "This is the content of the second post",
+        "published": False,
+        "rating": None,
+        "id": 2,
+    },
+]
+
+
 # request Get method URL: "/"
 @app.get("/")
 def read_root():
@@ -22,10 +41,14 @@ def read_root():
 # request Get method URL: "/posts"
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is a list posts"}
+    return {"data": my_posts}  # automatically serializes array into JSON
 
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_post(post: Post):
-    print(post.model_dump())  # This converts the pydantic model into a dictionary
-    return {"data": post}
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 100000)  # Temporarily add id until DB does it
+    # print(post.model_dump()) # # This converts the pydantic model into a dictionary
+    my_posts.append(post_dict)
+
+    return {"data": post_dict}
